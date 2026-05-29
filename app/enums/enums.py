25c256ac7838c86496c58f680e135c625060545b
sajithFdoDev <maheeshan@sisencodigital.com>
2026-05-29 11:27:25 +0530

    updated

--- app/enums/enums.py
+++ app/enums/enums.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
+
 class UserRole(str, Enum):
-    # Defines the security access control boundaries for internal staff roles
+    # Internal staff roles defining access control boundaries
     WHATSAPP_AGENT = "whatsapp_agent"
     CALL_CENTER_AGENT = "call_center_agent"
     PAYMENT_AGENT = "payment_agent"
@@ -9,23 +10,33 @@ class UserRole(str, Enum):
     MANAGER = "manager"
     CEO = "ceo"
 
+
 class TicketStage(str, Enum):
-    # Represents the sequential pipeline lifecycle states of a student inquiry
     NEW_LEAD = "New Lead"
+    DRAFTED = "Drafted"
+    DETAILS_COMPLETED = "Details Completed"
     QUALIFIED = "Qualified"
+    NOT_QUALIFIED = "Not Qualified"
     SENT_TO_CALL_CENTRE = "Sent to Call Centre"
-    CALLED = "Called"
+    CALL_NOT_ANSWERED = "Call Not Answered"
     CALL_RESCHEDULED = "Call Rescheduled"
-    FOLLOW_UP_IN_PROGRESS = "Follow-up in Progress"
+    TECHNICAL_FAULT = "Technical Fault"
+    WHATSAPP_MESSAGE_SENT = "WhatsApp Message Sent"
     INTERESTED = "Interested"
-    SALES_CLOSED = "Sales Closed"
-    PAYMENT_PENDING = "Payment Pending"
-    PAYMENT_VERIFIED = "Payment Verified"
-    ENROLLED = "Enrolled"
     NOT_INTERESTED = "Not Interested"
+    SALE_CONFIRMED = "Sale Confirmed"
+    WAIT_FOR_PAYMENT= "wait for payment"
+    CALLED_FOR_PAYMENT = "Called For Payment"
+    PAYMENT_COMPLETED = "Payment completed"
+    PAYMENT_VERIFIED = "Payment Verified"
+    PAYMENT_NOT_VERIFIED = "Payment Not Verified"
+    CALLED_FOR_PAYMENT_VERIFICATION = "Called For Payment Verification"
+    ENROLLED = "Enrolled"
+    LEAD_COMPLETED = "Lead Completed"
+
 
 class PaymentStatus(str, Enum):
-    # Tracks the manual review lifecycle of a third-party payment slip submission
+    # manual review lifecycle of a third-party payment slip submission
     PENDING = "Pending"
     VERIFIED = "Verified"
     REJECTED = "Rejected"
